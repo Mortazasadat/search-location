86ec9fc8c092b42da41376a53a7747621db05ce4
Sayed Murtaza <mortazasadat37@gmail.com>
2024-08-25 18:53:51 +0430

    mark img fixed

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,10 +3,22 @@ import "leaflet/dist/leaflet.css";
 import type { Map as leafletMap } from "leaflet";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { Place } from "@/types";
+import L from "leaflet";
 
 interface MapPorps {
   place: Place | null;
 }
+
+// Fix for default icon paths
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "/leaflet/marker-icon-2x.png",
+  iconUrl: "/leaflet/marker-icon.png",
+  shadowUrl: "/leaflet/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
 
 export default function MapCom({ place }: MapPorps) {
   const mapRef = useRef<leafletMap | null>(null);
